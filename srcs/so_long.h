94673e846df5eaeb37a58ff6f291321a92e578f1
adryeeel <adryelrbrz+github@gmail.com>
2024-09-17 00:56:44 +0100

    Fix include path

--- srcs/so_long.h
+++ srcs/so_long.h
@@ -6,14 +6,14 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 18:48:22 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/16 18:49:16 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/17 00:55:07 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 #define SO_LONG_H
 
-#include "libs/libmlx/mlx.h"
-#include "libs/libft/libft.h"
+#include "../libs/libmlx/mlx.h"
+#include "../libs/libft/libft.h"
 
 #endif
