71a195760f27ac40a3e7b8fb57751c09e07653ca
adryeeel <adryelrbrz+github@gmail.com>
2024-09-22 23:59:57 +0100

    Get the map component in enum form

---
+++ srcs/game/map/ft_map_component.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_map_component.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/20 17:48:45 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/09/20 17:50:57 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../so_long.h"
+
+t_mapc ft_map_component(char comp)
+{
+	if (comp == '0')
+		return (SPACE);
+
+	if (comp == '1')
+		return (WALL);
+
+	if (comp == 'C')
+		return (COLLECTIBLE);
+
+	if (comp == 'E')
+		return (EXIT_POINT);
+
+	if (comp == 'P')
+		return (START_POINT);
+
+	return (UNKNOWN);
+}
