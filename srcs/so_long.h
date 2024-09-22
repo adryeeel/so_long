062561ec10ced4d5f071decc205ee84eff6d5860
adryeeel <adryelrbrz+github@gmail.com>
2024-09-22 21:27:35 +0100

    Add stdbool and stdio libraries

--- srcs/so_long.h
+++ srcs/so_long.h
@@ -6,12 +6,15 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 18:48:22 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/22 21:25:44 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/22 21:27:04 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 #define SO_LONG_H
+
+#include <stdio.h>
+#include <stdbool.h>
 
 #include "../libs/libmlx/mlx.h"
 #include "../libs/libft/libft.h"
@@ -21,6 +24,5 @@ typedef struct s_xenv
 	void *window;
 	void *display;
 } t_xenv;
-
 
 #endif
