c2f6c1680e40c1ac9d6eb90b107e407eecbda881
adryeeel <adryelrbrz+github@gmail.com>
2024-09-22 22:30:21 +0100

    Add the xenv functions to the project's header file

--- srcs/so_long.h
+++ srcs/so_long.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 18:48:22 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/22 22:11:22 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/22 22:28:37 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,10 @@ int ft_hook_key(int key, void *param);
 
 bool ft_render_bg(void *display, t_ximg *scene);
 bool ft_render_scene(t_xenv x, t_game g, t_ximg *scene);
+
+/* Window Management — Xenv */
+
+void ft_xenv_free(t_xenv x);
+bool ft_xenv_setup(t_xenv *x);
 
 #endif
