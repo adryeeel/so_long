f3686765d19f1a250233a8bfdb1845716ccae452
adryeeel <adryelrbrz+github@gmail.com>
2024-09-22 21:58:17 +0100

    Add hook functions

--- srcs/so_long.h
+++ srcs/so_long.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 18:48:22 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/22 21:44:55 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/22 21:56:58 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,10 @@ typedef struct s_xenv
 
 bool ft_win_setup(t_xenv x, t_game g);
 int mlx_close_hook(void *win_ptr, int (*funct)(), void *param);
+
+/* Window Management — Hooks */
+
+int ft_hook_close(void *param);
+int ft_hook_key(int key, void *param);
 
 #endif
