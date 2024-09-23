49f04756a767574ae2ab90b6a4e1dafc207d7791
adryeeel <adryelrbrz+github@gmail.com>
2024-09-23 20:29:15 +0100

    Print a error message for X images problems

---
+++ srcs/error/print/ft_print_ximg.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_ximg.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 16:32:07 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/09/23 20:15:17 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../so_long.h"
+
+void ft_print_ximg(t_error err)
+{
+	if (err == ERR_XIMG_DATA)
+		ft_putendl_fd("Failed to create the X image: Faulty image buffer", STDERR_FILENO);
+
+	if (err == ERR_XIMG_PARAM)
+		ft_putendl_fd("Failed to create the X image: Invalid parameters", STDERR_FILENO);
+
+	if (err == ERR_XIMG_CREATE)
+		ft_putendl_fd("Failed to create the X image", STDERR_FILENO);
+
+	if (err == ERR_XIMG_FILEPATH)
+		perror("Failed to create the X image with XPM file");
+}
