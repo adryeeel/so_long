d61341d946006ca4506357860de11e5b4286bf53
adryeeel <adryelrbrz+github@gmail.com>
2024-09-23 20:46:22 +0100

    Change the window setup logic to handle error messages

--- srcs/win/ft_win_setup.c
+++ srcs/win/ft_win_setup.c
@@ -6,16 +6,18 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 22:12:35 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/22 16:47:11 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/23 18:18:37 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-bool ft_win_setup(t_xenv x, t_game g)
+t_error ft_win_setup(t_xenv x, t_game g)
 {
 	int win_w;
 	int win_h;
+
+	t_error err;
 	t_ximg scene;
 	t_param params;
 
@@ -25,7 +27,7 @@ bool ft_win_setup(t_xenv x, t_game g)
 	x.window = mlx_new_window(x.display, win_w, win_h, "so_long");
 
 	if (!x.window)
-		return (false);
+		return (ERR_WIN_CREATE);
 
 	params.g = &g;
 	params.x = &x;
@@ -33,12 +35,9 @@ bool ft_win_setup(t_xenv x, t_game g)
 	mlx_close_hook(x.window, ft_hook_close, &x);
 	mlx_key_hook(x.window, ft_hook_key, &params);
 
-	if (!ft_render_scene(x, g, &scene))
-	{
-		ft_xenv_free(x);
-		ft_game_free(g);
-		return (false);
-	}
+	err = ft_render_scene(x, g, &scene);
+	if (err)
+		return (err);
 
 	mlx_loop(x.display);
 
@@ -46,5 +45,5 @@ bool ft_win_setup(t_xenv x, t_game g)
 	ft_xenv_free(x);
 	ft_game_free(g);
 
-	return (true);
+	return (OK);
 }
