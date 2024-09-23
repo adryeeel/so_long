a7f54a16029a6965ea9f918abc2c1936f5c090f9
adryeeel <adryelrbrz+github@gmail.com>
2024-09-23 02:18:47 +0100

    So_long main function

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 18:11:40 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/16 18:11:59 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/23 02:17:51 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,23 @@
 
 int main(int argc, char const *argv[])
 {
-	(void)argc;
-	(void)argv;
-	return (0);
-}
+	char *map_path;
 
+	if (argc < 2)
+	{
+		ft_putendl_fd("Error: Map wasn't provided as argument", STDERR_FILENO);
+		return (EXIT_FAILURE);
+	}
+	if (argc > 2)
+	{
+		ft_putendl_fd("Error: Too many arguments", STDERR_FILENO);
+		return (EXIT_FAILURE);
+	}
+
+	map_path = (char *)argv[1];
+
+	if (!ft_game_start(map_path))
+		return (EXIT_FAILURE);
+
+	return (EXIT_SUCCESS);
+}
