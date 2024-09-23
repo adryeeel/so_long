b6f641f4718ab5f58a9d968696b3b7453966da41
adryeeel <adryelrbrz+github@gmail.com>
2024-09-23 20:47:49 +0100

    Refactor the function return to handle error messages

--- srcs/win/render/ft_render_scene.c
+++ srcs/win/render/ft_render_scene.c
@@ -6,30 +6,33 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 17:27:33 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/22 16:48:02 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/23 20:16:42 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
 
-bool ft_render_scene(t_xenv x, t_game g, t_ximg *scene)
+t_error ft_render_scene(t_xenv x, t_game g, t_ximg *scene)
 {
 	int win_h;
 	int win_w;
+	t_error err;
 
 	win_w = g.map.width * TILE_SIZE;
 	win_h = g.map.height * TILE_SIZE;
 
-	if (!ft_ximg_setup(x.display, scene, win_w, win_h))
-		return (false);
+	err = ft_ximg_setup(x.display, scene, win_w, win_h);
+	if (err)
+		return (ft_transl_rendscene(err));
 
-	if (!ft_render_bg(x.display, scene))
+	err = ft_render_bg(x.display, scene);
+	if (err)
 	{
 		ft_ximg_free(x.display, *scene);
-		return (false);
+		return (ft_transl_rendbg(err));
 	}
 
 	mlx_put_image_to_window(x.display, x.window, scene->id, 0, 0);
 
-	return (true);
+	return (OK);
 }
