02591df64da518ad2cb55e0fdff216ab4358f460
adryeeel <adryelrbrz+github@gmail.com>
2024-09-23 01:00:40 +0100

    Changed function's name

--- srcs/game/map/ft_map_process.c
+++ srcs/game/map/ft_map_process.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_emap_process.c                                  :+:      :+:    :+:   */
+/*   ft_map_process.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 16:12:47 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/23 00:40:24 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/23 00:45:49 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
 
-bool ft_emap_process(t_map *map, char *map_path)
+bool ft_map_process(t_map *map, char *map_path)
 {
 	char **raw_map;
 
