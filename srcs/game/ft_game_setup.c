854344d69b6377ff58007fd5eeec53eeb2726c66
adryeeel <adryelrbrz+github@gmail.com>
2024-09-23 20:43:41 +0100

    Change the game setup logic to handle error messages

--- srcs/game/ft_game_setup.c
+++ srcs/game/ft_game_setup.c
@@ -6,23 +6,25 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 14:25:35 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/22 15:50:41 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/23 14:39:42 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-bool ft_game_setup(t_game *g, char *map_path)
+t_error ft_game_setup(t_game *g, char *map_path)
 {
 	t_map map;
+	t_error err;
 
 	g->avatar.x = 0;
 	g->avatar.y = 0;
 
-	if (!ft_map_process(&map, map_path))
-		return (false);
+	err = ft_map_process(&map, map_path);
+	if (err)
+		return (err);
 
 	g->map = map;
 
-	return (true);
+	return (OK);
 }
