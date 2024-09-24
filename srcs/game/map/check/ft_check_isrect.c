918a28002b4bd6a7304dc63c09c01f94515e8125
adryeeel <adryelrbrz+github@gmail.com>
2024-09-24 15:29:26 +0100

    Check if the map is rectangular

---
+++ srcs/game/map/check/ft_check_isrect.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_isrect.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 14:45:37 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/09/24 14:55:45 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../so_long.h"
+
+bool ft_check_isrect(char *raw_map[])
+{
+	size_t width;
+	size_t height;
+
+	width = ft_strlen(raw_map[0]);
+	height = ft_strarr_length(raw_map);
+	if (height >= width)
+		return (ERR_MAP_RECT);
+	return (OK);
+}
