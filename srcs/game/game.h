88d831a293505a12447824a65c4087c2593992f5
adryeeel <adryelrbrz+github@gmail.com>
2024-09-25 00:16:33 +0100

    Add map header to game.h

--- srcs/game/game.h
+++ srcs/game/game.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:02:43 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/24 22:27:50 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/25 00:14:11 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,8 @@
 #define GAME_H
 
 #include <stdbool.h>
+#include "../map/map.h"
 #include "../error/error.h"
-
-typedef enum e_mapc
-{
-	WALL,
-	SPACE,
-	EXIT_POINT,
-	START_POINT,
-	COLLECTIBLE,
-	UNKNOWN,
-} t_mapc;
-
-typedef struct s_map
-{
-	int **matrix;
-	size_t width;
-	size_t height;
-} t_map;
 
 typedef struct s_coord
 {
@@ -50,35 +34,6 @@ typedef struct s_game
 void ft_game_free(t_game game);
 t_error ft_game_start(char *map_path);
 t_error ft_game_setup(t_game *g, char *map_path);
-
-/* Game — Map Manipulation */
-
-t_error ft_map_process(t_map *map, char *map_path);
-
-/* Game — Map Validation */
-
-t_error ft_check_ext(char *map_path);
-bool ft_check_isreg(char *raw_map[]);
-bool ft_check_isrect(char *raw_map[]);
-
-/* Game — Raw Map Manipulation */
-
-void ft_cmap_free(char *raw_map[]);
-int **ft_cmap_matrix(char *raw_map[]);
-t_error ft_cmap_check(char *raw_map[]);
-bool ft_cmap_push(char **raw_map[], char *line);
-t_error ft_cmap_read(char **raw_map[], char *map_path);
-
-/* Game — Enum Map Manipulation */
-
-t_error ft_emap_check(t_map *map);
-t_mapc ft_emap_component(char comp);
-t_error ft_emap_setup(t_map *map, char *raw_map[]);
-
-/* Game — Map Matrix Manipulation */
-
-void ft_matrix_free(int **matrix);
-int **ft_matrix_alloc(size_t x, size_t y);
 
 /* Game — Avatar's Position */
 
