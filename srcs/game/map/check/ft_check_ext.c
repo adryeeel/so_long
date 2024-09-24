b52f06c87046ab4902c8f47f562ad292014006d6
adryeeel <adryelrbrz+github@gmail.com>
2024-09-24 22:32:29 +0100

    Check if the map path has valid extension and it's not a directory

--- srcs/game/map/check/ft_check_ext.c
+++ srcs/game/map/check/ft_check_ext.c
@@ -6,14 +6,25 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 14:45:37 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/24 15:25:05 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/24 22:27:31 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../so_long.h"
 
-bool ft_check_ext(char *map_path)
+t_error ft_check_ext(char *map_path)
 {
-	(void)map_path;
-	return (true);
+	char *ext;
+
+	if (map_path[ft_strlen(map_path) - 1] == '/')
+		return (ERR_MAP_ISDIR);
+
+	ext = ft_strrchr(map_path, '.');
+	if (!ext)
+		return (ERR_MAP_EXT);
+
+	if (ft_strncmp(ext + 1, "ber", ft_strlen(ext)) != 0)
+		return (ERR_MAP_EXT);
+
+	return (OK);
 }
