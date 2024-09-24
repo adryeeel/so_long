eac8f0ccc9c8d94fc9ebdef0f3e58a24693a65fb
adryeeel <adryelrbrz+github@gmail.com>
2024-09-25 00:16:59 +0100

    Add function to parse map flags

--- srcs/map/emap/ft_emap_check.c
+++ srcs/map/emap/ft_emap_check.c
@@ -6,13 +6,31 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 15:43:34 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/24 22:40:24 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/25 00:15:12 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
 
-t_error ft_emap_check(t_map *map)
+// A valid
+// Valid componenets
+// One exit, one start and at least one collectible
+// Must be surrounded by walls
+
+t_flags ft_parse_flags(t_map map)
+{
+	t_flags flags;
+
+	flags.exit = 0;
+	flags.start = 0;
+	flags.unknown = 0;
+	flags.collectible = 0;
+
+	(void)map;
+	return (flags);
+}
+
+t_error ft_emap_check(t_map map)
 {
 	(void)map;
 	return (OK);
