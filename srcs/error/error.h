d5514e35d187f8569d7cbe8ee4d5fb055b768e03
adryeeel <adryelrbrz+github@gmail.com>
2024-09-24 22:30:26 +0100

    Add ERR_MAP_ISDIR error type

--- srcs/error/error.h
+++ srcs/error/error.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:00:03 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/24 14:56:06 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/24 22:26:51 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,11 @@ typedef enum e_error
 	ERR_WIN_CREATE,
 	ERR_BAD_ARGS,
 	ERR_MAP_READ,
+	ERR_MAP_EXT,
 	ERR_MAP_ALLOC,
 	ERR_MAP_EMPTY,
 	ERR_MAP_RECT,
+	ERR_MAP_ISDIR,
 	ERR_MAP_REG,
 	ERR_MAP_MATRIX,
 	ERR_XIMG_DATA,
