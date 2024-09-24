17170c86cdb79af323f03aaeb73d7b4de22b44c0
adryeeel <adryelrbrz+github@gmail.com>
2024-09-24 14:40:16 +0100

    Refactor to handle errors for empty maps and bad alloc

--- srcs/game/map/cmap/ft_cmap_read.c
+++ srcs/game/map/cmap/ft_cmap_read.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 00:33:56 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/23 01:59:32 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/24 14:34:51 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,35 +21,50 @@ static void ft_strtrim_nl(char **line)
 	*line = trimmed;
 }
 
-char **ft_cmap_read(char *map_path)
+static t_error ft_read_setup(char **raw_map[], int *map_fd, char *map_path)
+{
+	if (!*raw_map)
+	{
+		*raw_map = ft_calloc(1, sizeof(char *));
+		if (!*raw_map)
+			return (ERR_MAP_ALLOC);
+	}
+	*map_fd = open(map_path, O_RDONLY);
+	if (*map_fd < 0)
+	{
+		free(*raw_map);
+		return (ERR_MAP_READ);
+	}
+	return (OK);
+}
+
+t_error ft_cmap_read(char **raw_map[], char *map_path)
 {
 	int map_fd;
 	char *line;
-	char **raw_map;
+	t_error err;
+	
+	map_fd = 0;
+	err = ft_read_setup(raw_map, &map_fd, map_path);
+	if (err)
+		return (err);
 
-	raw_map = ft_calloc(1, sizeof(char *));
-
-	if (!raw_map)
-		return (NULL);
-
-	map_fd = open(map_path, O_RDONLY);
 	line = ft_readl(map_fd);
-
-	if (!line || map_fd < 0)
+	if (!line)
 	{
-		free(raw_map);
-		return (NULL);
+		free(*raw_map);
+		close(map_fd);
+		return (ERR_MAP_EMPTY);
 	}
 
 	while (line)
 	{
 		ft_strtrim_nl(&line);
-		ft_cmap_push(&raw_map, line);
+		ft_cmap_push(raw_map, line);
 		free(line);
 		line = ft_readl(map_fd);
 	}
 
 	close(map_fd);
-
-	return (raw_map);
+	return (OK);
 }
