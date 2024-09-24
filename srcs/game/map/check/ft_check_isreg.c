77c4487cd45e137bc7ff17e9577f1d2e50c24602
adryeeel <adryelrbrz+github@gmail.com>
2024-09-24 15:29:40 +0100

    Check if all map rows are uniform

---
+++ srcs/game/map/check/ft_check_isreg.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_isreg.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 14:45:37 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/09/24 14:55:56 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../so_long.h"
+
+bool ft_check_isreg(char *raw_map[])
+{
+	size_t i;
+	size_t row;
+	size_t width;
+	size_t height;
+
+	width = ft_strlen(raw_map[0]);
+	height = ft_strarr_length(raw_map);
+
+	i = 1;
+	while (i < height)
+	{
+		row = ft_strlen(raw_map[i]);
+		if (row != width)
+			return (ERR_MAP_REG);
+		i++;
+	}
+	return (OK);
+}
