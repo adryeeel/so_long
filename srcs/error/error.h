35e68dc162cc52a8fb6fcc9165eeba9f0ba59ab0
adryeeel <adryelrbrz+github@gmail.com>
2024-09-25 02:22:15 +0100

    Add new errors for map format

--- srcs/error/error.h
+++ srcs/error/error.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:00:03 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/24 22:26:51 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/25 01:13:52 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,35 @@ typedef enum e_error
 	ERR_XSRV_INIT,
 	ERR_WIN_CREATE,
 	ERR_BAD_ARGS,
-	ERR_MAP_READ,
+
+	/* Map Setup Errors */
 	ERR_MAP_EXT,
+	ERR_MAP_READ,
 	ERR_MAP_ALLOC,
 	ERR_MAP_EMPTY,
-	ERR_MAP_RECT,
 	ERR_MAP_ISDIR,
-	ERR_MAP_REG,
 	ERR_MAP_MATRIX,
+
+	/* Map Format Errors */
+	ERR_MAP_REG,
+	ERR_MAP_EXIT,
+	ERR_MAP_RECT,
+	ERR_MAP_START,
+	ERR_MAP_UNKNOWN,
+	ERR_MAP_SURRONDED,
+	ERR_MAP_COLLECTIBLE,
+
+	/* X Images Errors */
 	ERR_XIMG_DATA,
 	ERR_XIMG_PARAM,
 	ERR_XIMG_CREATE,
 	ERR_XIMG_FILEPATH,
+	
+	/* Scene Render Errors */
 	ERR_RENDSCENE_DATA,
 	ERR_RENDSCENE_CREATE,
+	
+	/* Background Render Errors */
 	ERR_RENDBG_CREATE,
 	ERR_RENDBG_DATA,
 	ERR_RENDBG_FILEPATH,
