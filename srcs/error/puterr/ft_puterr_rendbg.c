111ea195d10f9970570c6646a1304a9df78bb37d
adryeeel <adryelrbrz+github@gmail.com>
2024-09-25 02:37:01 +0100

    Fix functions call to the new name standard

--- srcs/error/puterr/ft_puterr_rendbg.c
+++ srcs/error/puterr/ft_puterr_rendbg.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 16:32:07 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/25 02:31:48 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/25 02:35:08 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void ft_puterr_rendbg(t_error err)
 	ft_putendl_fd("Unable to render the game's background", STDERR_FILENO);
 	
 	if (err == ERR_RENDBG_CREATE)
-		ft_print_ximg(ERR_XIMG_CREATE);
+		ft_puterr_ximg(ERR_XIMG_CREATE);
 
 	if (err == ERR_RENDBG_DATA)
-		ft_print_ximg(ERR_XIMG_DATA);
+		ft_puterr_ximg(ERR_XIMG_DATA);
 
 	if (err == ERR_RENDBG_FILEPATH)
-		ft_print_ximg(ERR_XIMG_FILEPATH);
+		ft_puterr_ximg(ERR_XIMG_FILEPATH);
 }
--- srcs/error/puterr/ft_puterr_rendscene.c
+++ srcs/error/puterr/ft_puterr_rendscene.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 16:32:07 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/25 02:31:44 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/25 02:35:13 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void ft_puterr_rendscene(t_error err)
 	ft_putendl_fd("Unable to render the game's scene", STDERR_FILENO);
 	
 	if (err == ERR_RENDSCENE_CREATE)
-		ft_print_ximg(ERR_XIMG_CREATE);
+		ft_puterr_ximg(ERR_XIMG_CREATE);
 
 	if (err == ERR_RENDSCENE_DATA)
-		ft_print_ximg(ERR_XIMG_DATA);
+		ft_puterr_ximg(ERR_XIMG_DATA);
 }
