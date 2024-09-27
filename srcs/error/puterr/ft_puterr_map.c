fb2d956ba6a90c53326aca0057b35e6751ad2a9a
adryeeel <adryelrbrz+github@gmail.com>
2024-09-27 23:14:08 +0100

    Add error messages for map path

--- srcs/error/puterr/ft_puterr_map.c
+++ srcs/error/puterr/ft_puterr_map.c
@@ -6,17 +6,29 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 16:32:07 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/25 02:30:04 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/27 14:01:46 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
 
+static void ft_puterr_map_path(t_error err)
+{
+	if (err == ERR_MAP_PATH_ALLOC)
+		perror("Failed to alloc during map path checking");
+
+	if (err == ERR_MAP_PATH_EXIT)
+		ft_putendl_fd("Invalid map path: Exit point is not reachable", STDERR_FILENO);
+
+	if (err == ERR_MAP_PATH_COLL)
+		ft_putendl_fd("Invalid map path: Collectible is not reachable", STDERR_FILENO);
+}
+
 static void ft_puterr_map_setup(t_error err)
 {
 	if (err == ERR_MAP_EXT)
 		ft_putendl_fd("Invalid map: Bad file extension", STDERR_FILENO);
-		
+
 	if (err == ERR_MAP_READ)
 		perror("Failed to read the map");
 
@@ -64,4 +76,7 @@ void ft_puterr_map(t_error err)
 
 	if (err >= ERR_MAP_REG && err <= ERR_MAP_COLLECTIBLE)
 		ft_puterr_map_format(err);
+
+	if (err >= ERR_MAP_PATH_EXIT && err <= ERR_MAP_PATH_ALLOC)
+		ft_puterr_map_path(err);
 }
