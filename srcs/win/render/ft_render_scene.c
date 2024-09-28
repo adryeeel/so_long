90cf264deab7e2c545bfbb265b5f4e04e1dbce43
adryeeel <adryelrbrz+github@gmail.com>
2024-09-28 17:07:59 +0100

    Refactor the function name that handle errors

--- srcs/win/render/ft_render_scene.c
+++ srcs/win/render/ft_render_scene.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 17:27:33 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/23 20:16:42 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/28 16:30:47 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ t_error ft_render_scene(t_xenv x, t_game g, t_ximg *scene)
 
 	err = ft_ximg_setup(x.display, scene, win_w, win_h);
 	if (err)
-		return (ft_transl_rendscene(err));
+		return (ft_error_scene(err));
 
 	err = ft_render_bg(x.display, scene);
 	if (err)
 	{
 		ft_ximg_free(x.display, *scene);
-		return (ft_transl_rendbg(err));
+		return (ft_error_bg(err));
 	}
 
 	mlx_put_image_to_window(x.display, x.window, scene->id, 0, 0);
