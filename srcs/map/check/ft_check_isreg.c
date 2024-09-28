5e5b2b621921a82b00400fa757a293df194e66a4
adryeeel <adryelrbrz+github@gmail.com>
2024-09-28 13:37:17 +0100

    Check if all map rows are regular

--- srcs/map/check/ft_check_isreg.c
+++ srcs/map/check/ft_check_isreg.c
@@ -6,27 +6,22 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 14:45:37 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/24 22:40:24 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/28 03:33:56 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
 
-bool ft_check_isreg(char *raw_map[])
+bool ft_check_isreg(t_map map)
 {
 	size_t i;
 	size_t row;
-	size_t width;
-	size_t height;
-
-	width = ft_strlen(raw_map[0]);
-	height = ft_strarr_length(raw_map);
 
 	i = 1;
-	while (i < height)
+	while (i < map.height)
 	{
-		row = ft_strlen(raw_map[i]);
-		if (row != width)
+		row = ft_strlen(map.grid[i]);
+		if (row != map.width)
 			return (false);
 		i++;
 	}
