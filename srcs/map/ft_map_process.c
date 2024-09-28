0bc3cfcfed9cf552355ee09cdb496894855d29a7
adryeeel <adryelrbrz+github@gmail.com>
2024-09-28 13:41:42 +0100

    Process the map file

--- srcs/map/ft_map_process.c
+++ srcs/map/ft_map_process.c
@@ -6,65 +6,35 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 16:12:47 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/25 00:08:10 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/28 04:27:37 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-static t_error ft_cmap_process(char **raw_map[], char *map_path)
+t_error ft_map_process(t_map *map, char *map_path)
 {
 	t_error err;
+	char **grid;
+
+	grid = NULL;
 
 	err = ft_check_ext(map_path);
 	if (err)
 		return (err);
 
-	err = ft_cmap_read(raw_map, map_path);
+	err = ft_map_read(&grid, map_path);
 	if (err)
 		return (err);
 
-	err = ft_cmap_check(*raw_map);
+	ft_map_setup(map, grid);
+
+	err = ft_map_check(*map);
 	if (err)
 	{
-		ft_cmap_free(*raw_map);
-		return (err);
-	}
-	return (OK);
-}
-
-static t_error ft_emap_process(t_map *map, char *raw_map[])
-{
-	t_error err;
-
-	err = ft_emap_setup(map, raw_map);
-	if (err)
-		return (err);
-
-	err = ft_emap_check(*map);
-	if (err)
-	{
-		ft_matrix_free(map->matrix);
+		ft_map_free(grid);
 		return (err);
 	}
 
-	return (OK);
-}
-
-t_error ft_map_process(t_map *map, char *map_path)
-{
-	t_error err;
-	char **raw_map;
-
-	raw_map = NULL;
-	err = ft_cmap_process(&raw_map, map_path);
-	if (err)
-		return (err);
-
-	err = ft_emap_process(map, raw_map);
-	ft_cmap_free(raw_map);
-
-	if (err)
-		return (err);
 	return (OK);
 }
