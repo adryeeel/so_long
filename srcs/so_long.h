b321c8837ce32ce4c4c7009d1e640376b5bedeb2
adryeeel <adryelrbrz+github@gmail.com>
2024-09-29 02:23:03 +0100

    Update asssets path

--- srcs/so_long.h
+++ srcs/so_long.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 18:48:22 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/27 22:44:38 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/28 23:27:41 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,9 @@
 
 #define TILE_SIZE 32
 #define TOTAL_DIRECTIONS 4
+
 #define BG_IMG_PATH "assets/imgs/bg.xpm"
+#define WALL_IMG_PATH "assets/imgs/wall.xpm"
 #define AVATAR_IMG_PATH "assets/imgs/avatar.xpm"
 
 /* Matrix Manipulation */
