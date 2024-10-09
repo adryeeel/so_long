990e8f9c4ac0da84a1b6a75e61bf3b2a3287ac3c
adryeeel <adryelrbrz+github@gmail.com>
2024-10-09 23:04:11 +0100

    Copy X image to X image based on dst_p coords

--- srcs/win/ximg/ft_ximg_copy.c
+++ srcs/win/ximg/ft_ximg_copy.c
@@ -6,48 +6,33 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 17:24:02 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/30 21:55:09 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/07 01:55:36 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
 
-#define TRANSPARENT (char)0xFF000000
+#define PXL_SIZE 4
 
-#define SRC_ROW_IDX (y * src.size_line)
-#define SRC_COL_IDX (x * (src.bpp / 8))
+#define SRC_PXL_COL x * (src.bpp / 8)
+#define SRC_PXL_ROW y * src.size_line
 
-#define DST_ROW_IDX ((dst_p.y + y) * dst->size_line)
-#define DST_COL_IDX ((dst_p.x + x) * (dst->bpp / 8))
+#define DST_PXL_COL (x + dst_p.x) * (dst->bpp / 8)
+#define DST_PXL_ROW (y + dst_p.y) * dst->size_line
+
+#define DST_PXL DST_PXL_ROW + DST_PXL_COL
+#define SRC_PXL SRC_PXL_ROW + SRC_PXL_COL
 
 void ft_ximg_copy(t_ximg *dst, t_ximg src, t_coord dst_p)
 {
 	int x;
 	int y;
-	size_t dst_idx;
-	size_t src_idx;
 
-	y = 0;
-	while (y < src.height)
+	y = -1;
+	while (++y < src.height)
 	{
-		x = 0;
-		while (x < src.width)
-		{
-			dst_idx = DST_ROW_IDX + DST_COL_IDX;
-			src_idx = SRC_ROW_IDX + SRC_COL_IDX;
-
-			if (src.buff[src_idx] == TRANSPARENT)
-			{
-				x++;
-				continue;
-			}
-
-			dst->buff[dst_idx] = src.buff[src_idx];
-			dst->buff[dst_idx + 1] = src.buff[src_idx + 1];
-			dst->buff[dst_idx + 2] = src.buff[src_idx + 2];
-			dst->buff[dst_idx + 3] = src.buff[src_idx + 3];
-			x++;
-		}
-		y++;
+		x = -1;
+		while (++x < src.width)
+			ft_memmove(dst->buff + DST_PXL, src.buff + SRC_PXL, PXL_SIZE);
 	}
 }
