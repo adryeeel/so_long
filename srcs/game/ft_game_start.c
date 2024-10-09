0b15fbc1e3776a2cf3b97ef32f83c36416dfc5a8
adryeeel <adryelrbrz+github@gmail.com>
2024-10-09 22:48:48 +0100

    Refactor to add game's loop and args struct

--- srcs/game/ft_game_start.c
+++ srcs/game/ft_game_start.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 02:13:33 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/29 20:24:36 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/07 01:10:00 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,36 @@
 t_error ft_game_start(char *map_path)
 {
 	t_xenv x;
-	t_game game;
+	t_game g;
 	t_error err;
+	t_param args;
 
-	err = ft_game_setup(&game, map_path);
+	err = ft_game_setup(&g, map_path);
 	if (err)
 		return (err);
-	
-	err = ft_xenv_setup(&x, game.map);
+
+	err = ft_xenv_setup(&x, g.map);
 	if (err)
 	{
-		ft_game_free(game);
+		ft_game_free(g);
 		return (err);
 	}
 
-	err = ft_win_setup(&x, game);
+	args.x = &x;
+	args.g = &g;
+
+	err = ft_win_setup(&x, g, &args);
 	if (err)
 	{
 		ft_xenv_free(x);
-		ft_game_free(game);
+		ft_game_free(g);
 		return (err);
 	}
+
+	mlx_loop(x.display);
+
+	ft_xenv_free(x);
+	ft_game_free(g);
+
 	return (OK);
 }
