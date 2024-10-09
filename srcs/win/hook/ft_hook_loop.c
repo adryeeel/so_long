75650aaf80819737fff5de68d94cff09519466c2
adryeeel <adryelrbrz+github@gmail.com>
2024-10-09 23:01:36 +0100

    Render animations

--- srcs/win/hook/ft_hook_loop.c
+++ srcs/win/hook/ft_hook_loop.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 19:08:57 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/30 22:25:50 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/08 01:36:29 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,18 +29,17 @@ int ft_hook_loop(void *param)
 	t_xenv *x;
 	t_game *g;
 	t_error err;
-	t_param *data;
-	static size_t last_time = 0;
-	size_t curr_time = ft_draw_time();
+	size_t curr_time;
+	static size_t last_time;
 
-	data = (t_param *)param;
-	x = data->x;
-	g = data->g;
+	x = ((t_param *)param)->x;
+	g = ((t_param *)param)->g;
+	curr_time = ft_draw_time();
 
 	if (curr_time - last_time < FPS_16)
 		return (OK);
 
-	err = ft_draw_anim(*x, *g);
+	err = ft_draw_anim(*x, g->map);
 	if (err)
 	{
 		ft_error_print(err);
