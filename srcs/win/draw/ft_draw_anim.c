1acc65f3978c1cbfdff9a3035aa6f7ef520d4e0f
adryeeel <adryelrbrz+github@gmail.com>
2024-10-09 22:56:55 +0100

    Draw patrol and collectibles animation

--- srcs/win/draw/ft_draw_anim.c
+++ srcs/win/draw/ft_draw_anim.c
@@ -6,15 +6,42 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 00:39:11 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/30 22:21:49 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/09 21:02:11 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
 
-#define COLL_FRAME_N frame_n % (x.coll.width / TILE_SIZE)
+static t_error ft_draw_frame(t_xenv x, t_map map, t_mapc comp, size_t frame_n)
+{
+	t_error err;
+	t_ximg frame;
+	t_coord coord;
+	t_ximg frameset;
 
-t_error ft_draw_anim(t_xenv x, t_game g)
+	if (comp == PATROL)
+		frameset = x.patrol;
+
+	if (comp == COLLECTIBLE)
+		frameset = x.coll;
+
+	err = ft_ximg_frame(x.display, frameset, &frame, frame_n);
+	if (err)
+		return (err);
+
+	coord = ft_map_search_seq(map, comp);
+	while (coord.x >= 0)
+	{
+		ft_draw_background(x.display, &frame);
+		ft_draw_at(&x.scene, frame, coord);
+		coord = ft_map_search_seq(map, comp);
+	}
+
+	ft_ximg_free(x.display, frame);
+	return (OK);
+}
+
+t_error ft_draw_anim(t_xenv x, t_map map)
 {
 	t_error err;
 	static size_t frame_n;
@@ -22,11 +49,11 @@ t_error ft_draw_anim(t_xenv x, t_game g)
 	if (frame_n >= 12)
 		frame_n = 0;
 
-	err = ft_draw_move(x, g.map, g.avatar);
+	err = ft_draw_frame(x, map, PATROL, frame_n);
 	if (err)
-		return (ft_error_avatar(err));
+		return (ft_error_patrol(err));
 
-	err = ft_draw_coll(x, g, COLL_FRAME_N);
+	err = ft_draw_frame(x, map, COLLECTIBLE, frame_n);
 	if (err)
 		return (ft_error_coll(err));
 
