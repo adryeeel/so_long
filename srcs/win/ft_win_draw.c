bdf3bd1540406846b53a08246fb83689958154ea
adryeeel <adryelrbrz+github@gmail.com>
2024-10-09 23:08:47 +0100

    Draw static components to the game's scene

---
+++ srcs/win/ft_win_draw.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_win_draw.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 00:49:08 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/10/09 22:28:06 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+
+t_error ft_win_draw(t_xenv x, t_game g)
+{
+	t_error err;
+
+	err = ft_draw_comp(x, g, WALL);
+	if (err)
+		return (ft_error_wall(err));
+
+	err = ft_draw_comp(x, g, SPACE);
+	if (err)
+		return (ft_error_space(err));
+
+	err = ft_draw_comp(x, g, AVATAR);
+	if (err)
+		return (ft_error_avatar(err));
+
+	err = ft_draw_comp(x, g, EXIT_POINT);
+	if (err)
+		return (ft_error_exit(err));
+
+	return (OK);
+}
