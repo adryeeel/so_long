3170cb97dae0cb8961a3d2e72e7c050a08d8261d
adryeeel <adryelrbrz+github@gmail.com>
2024-10-09 23:05:14 +0100

    Extract a n frame from a frameset

--- srcs/win/ximg/ft_ximg_frame.c
+++ srcs/win/ximg/ft_ximg_frame.c
@@ -6,35 +6,37 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 14:00:37 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/30 14:00:53 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/08 13:49:39 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
 
-#define DST_ROW y_idx * frame->size_line
-#define SRC_ROW y_idx * frameset.size_line
-#define SRC_COL frame_n * frameset.bpp / 8 * TILE_SIZE
+#define FRSET_COL n * frame->size_line
+#define FRSET_ROW y * frameset.size_line
+#define FRSET_OFF FRSET_ROW + FRSET_COL
 
-#define DST_OFFSET frame->buff + DST_ROW
-#define SRC_OFFSET frameset.buff + SRC_ROW + SRC_COL
+#define FRAME_COL 0
+#define FRAME_ROW y * frame->size_line
+#define FRAME_OFF FRAME_ROW + FRAME_COL
 
-#define BYTES_PER_LINE frameset.bpp / 8 * TILE_SIZE
-
-t_error ft_ximg_frame(t_xenv x, t_ximg frameset, t_ximg *frame, size_t frame_n)
+t_error ft_ximg_frame(void *display, t_ximg frameset, t_ximg *frame, size_t n)
 {
-	int y_idx;
+	int y;
 	t_error err;
 
-	err = ft_ximg_setup(x.display, frame, TILE_SIZE, TILE_SIZE);
+	err = ft_ximg_setup(display, frame, TILE_SIZE, TILE_SIZE);
 	if (err)
 		return (err);
 
-	y_idx = 0;
-	while (y_idx < frame->height)
+	y = -1;
+	while (++y < frame->height)
 	{
-		ft_memmove(DST_OFFSET, SRC_OFFSET, BYTES_PER_LINE);
-		y_idx++;
+		ft_memmove(
+			frame->buff + FRAME_OFF, 
+			frameset.buff + FRSET_OFF, 
+			frame->size_line
+		);
 	}
 
 	return (OK);
