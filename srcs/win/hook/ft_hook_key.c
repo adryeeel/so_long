6da1bcdd489fb6255beeb7b0bfacf4cf25f13882
adryeeel <adryelrbrz+github@gmail.com>
2024-10-09 23:00:47 +0100

    Add params destructuring

--- srcs/win/hook/ft_hook_key.c
+++ srcs/win/hook/ft_hook_key.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 20:17:06 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/30 22:19:02 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/07 01:09:11 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,9 @@ int ft_hook_key(int key, void *param)
 {
 	t_xenv *x;
 	t_game *g;
-	t_param *data;
 
-	data = (t_param *)param;
-	x = data->x;
-	g = data->g;
+	x = ((t_param *)param)->x;
+	g = ((t_param *)param)->g;
 
 	if (key == XK_Escape)
 		mlx_loop_end(x->display);
@@ -41,6 +39,9 @@ int ft_hook_key(int key, void *param)
 
 	if (key == XK_D || key == XK_d)
 		ft_move_right(g);
+
+	ft_draw_move(*x, *g);
+	mlx_put_image_to_window(x->display, x->window, x->scene.id, 0, TILE_SIZE);
 
 	return (0);
 }
