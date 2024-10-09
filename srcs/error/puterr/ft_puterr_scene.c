ee39a31d63756da8b7e06c78b963d04736199ebb
adryeeel <adryelrbrz+github@gmail.com>
2024-10-09 22:46:33 +0100

    Print error messages related to the map's scene render

--- srcs/error/puterr/ft_puterr_scene.c
+++ srcs/error/puterr/ft_puterr_scene.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 16:32:07 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/28 16:26:24 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/09 21:56:23 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void ft_puterr_scene(t_error err)
 {
-	ft_putendl_fd("Unable to render the game's scene", STDERR_FILENO);
+	ft_putendl_fd("Unable to create the game's scene", STDERR_FILENO);
 
 	if (err == ERR_RENDSCENE_PARAM)
 		ft_puterr_ximg(ERR_XIMG_PARAM);
