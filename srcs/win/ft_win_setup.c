5a94e6385e1d09219885ad4f025a765da31fdb66
adryeeel <adryelrbrz+github@gmail.com>
2024-10-09 23:09:12 +0100

    Refactor function to make it simpler

--- srcs/win/ft_win_setup.c
+++ srcs/win/ft_win_setup.c
@@ -6,41 +6,25 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 22:12:35 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/30 22:23:59 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/09 22:22:14 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-t_error ft_win_setup(t_xenv *x, t_game g)
+#define WIN_WH g.map.width *TILE_SIZE
+#define WIN_HT (g.map.height + 1) * TILE_SIZE
+
+t_error ft_win_setup(t_xenv *x, t_game g, t_param *args)
 {
-	int win_w;
-	int win_h;
-	t_error err;
-	t_param params;
+	x->window = mlx_new_window(x->display, WIN_WH, WIN_HT, "so_long");
 
-	win_w = g.map.width * TILE_SIZE;
-	win_h = (g.map.height + 1) * TILE_SIZE;
-
-	x->window = mlx_new_window(x->display, win_w, win_h, "so_long");
 	if (!x->window)
 		return (ERR_WIN_CREATE);
 
-	err = ft_draw_static(*x, g);
-	if (err)
-		return (err);
-
-	params.g = &g;
-	params.x = x;
-
+	mlx_key_hook(x->window, ft_hook_key, args);
 	mlx_close_hook(x->window, ft_hook_close, x);
-	mlx_key_hook(x->window, ft_hook_key, &params);
-	mlx_loop_hook(x->display, ft_hook_loop, &params);
+	mlx_loop_hook(x->display, ft_hook_loop, args);
 
-	mlx_loop(x->display);
-
-	ft_xenv_free(*x);
-	ft_game_free(g);
-
-	return (OK);
+	return (ft_win_draw(*x, g));
 }
