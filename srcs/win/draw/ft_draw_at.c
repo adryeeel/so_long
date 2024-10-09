62eb4d49186ca36243bcee78aa62dc82e8ad642c
adryeeel <adryelrbrz+github@gmail.com>
2024-10-09 22:57:44 +0100

    Draw sprite at X and Y coordinates in a X image

---
+++ srcs/win/draw/ft_draw_at.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_draw_at.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 17:07:19 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/10/07 17:10:51 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../so_long.h"
+
+void ft_draw_at(t_ximg *dst, t_ximg src, t_coord coord)
+{
+	ft_ximg_copy(dst, src, (t_coord){
+		coord.x * TILE_SIZE, 
+		coord.y * TILE_SIZE
+	});
+}
+
