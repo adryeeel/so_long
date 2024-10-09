9f84d468ef4c6fe918f3baec6ae69d3e3380e537
adryeeel <adryelrbrz+github@gmail.com>
2024-10-09 23:02:48 +0100

    Add free to patrol frameset

--- srcs/win/xenv/ft_xenv_free.c
+++ srcs/win/xenv/ft_xenv_free.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 23:04:50 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/30 01:53:29 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/07 22:18:55 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,9 @@ void ft_xenv_free(t_xenv x)
 
 	if (x.scene.id)
 		ft_ximg_free(x.display, x.scene);
+
+	if (x.patrol.id)
+		ft_ximg_free(x.display, x.patrol);
 
 	if (x.coll.id)
 		ft_ximg_free(x.display, x.coll);
