cab2d85d8766ec9634b6240a3884e6e8461c25ea
adryeeel <adryelrbrz+github@gmail.com>
2024-10-10 15:30:25 +0100

    Consider avatar orientation to load the correct sprite

--- srcs/win/ximg/ft_ximg_sprite.c
+++ srcs/win/ximg/ft_ximg_sprite.c
@@ -6,22 +6,25 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 17:38:29 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/08 01:16:56 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/10 15:27:25 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
 
-t_error ft_ximg_sprite(t_xenv x, t_ximg *tex, t_mapc comp)
+t_error ft_ximg_sprite(t_xenv x, bool orient, t_ximg *tex, t_mapc comp)
 {
+	if (comp == AVATAR && orient == RIGHT)
+		return (ft_ximgf_setup(x.display, tex, AVATAR_IMG_PATH));
+
+	if (comp == AVATAR)
+		return (ft_ximgf_setup(x.display, tex, LAVATAR_IMG_PATH));
+
 	if (comp == SPACE)
 		return (ft_ximgf_setup(x.display, tex, SPACE_IMG_PATH));
 
 	if (comp == WALL)
 		return (ft_ximgf_setup(x.display, tex, WALL_IMG_PATH));
-
-	if (comp == AVATAR)
-		return (ft_ximgf_setup(x.display, tex, AVATAR_IMG_PATH));
 
 	if (comp == EXIT_POINT)
 		return (ft_ximgf_setup(x.display, tex, EXIT_IMG_PATH));
