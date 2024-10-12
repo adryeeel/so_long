02b82517450315144271ef947e1ccf8d0982128e
adryeeel <adryelrbrz+github@gmail.com>
2024-10-13 00:49:06 +0100

    Fix image free argument

--- srcs/win/xenv/ft_xenv_free.c
+++ srcs/win/xenv/ft_xenv_free.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 23:04:50 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/13 00:10:54 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/13 00:48:19 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static void ft_sprite_free(t_xenv x)
 		ft_ximg_free(x.display, x.coll);
 
 	if (x.death.id)
-		ft_ximg_free(x.display, x.coll);
+		ft_ximg_free(x.display, x.death);
 }
 
 void ft_xenv_free(t_xenv x)
