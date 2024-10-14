8d31b182996e8b26da3cd93141edf15753f1425f
adryeeel <adryelrbrz+github@gmail.com>
2024-10-14 17:32:15 +0100

    Set avatar death flag

--- srcs/game/move/ft_move_right.c
+++ srcs/game/move/ft_move_right.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 20:04:50 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/10 15:22:27 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/14 16:53:58 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,12 @@ void ft_move_right(t_game *g)
 
 	if (g->map.grid[y][x] == WALL)
 		return;
+
+	if (g->map.grid[y][x] == PATROL)
+	{
+		g->avatar.died = true;
+		return;
+	}
 
 	if (g->map.grid[y][x] == COLLECTIBLE)
 		g->map.grid[y][x] = SPACE;
