f7ff97605509a5dc82265bf162f3e501ec5d297f
adryeeel <adryelrbrz+github@gmail.com>
2024-10-14 17:34:19 +0100

    Update ft_draw_frame call parameters

--- srcs/win/draw/ft_draw_anim.c
+++ srcs/win/draw/ft_draw_anim.c
@@ -6,42 +6,13 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 00:39:11 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/09 21:02:11 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/14 16:35:08 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
 
-static t_error ft_draw_frame(t_xenv x, t_map map, t_mapc comp, size_t frame_n)
-{
-	t_error err;
-	t_ximg frame;
-	t_coord coord;
-	t_ximg frameset;
-
-	if (comp == PATROL)
-		frameset = x.patrol;
-
-	if (comp == COLLECTIBLE)
-		frameset = x.coll;
-
-	err = ft_ximg_frame(x.display, frameset, &frame, frame_n);
-	if (err)
-		return (err);
-
-	coord = ft_map_search_seq(map, comp);
-	while (coord.x >= 0)
-	{
-		ft_draw_background(x.display, &frame);
-		ft_draw_at(&x.scene, frame, coord);
-		coord = ft_map_search_seq(map, comp);
-	}
-
-	ft_ximg_free(x.display, frame);
-	return (OK);
-}
-
-t_error ft_draw_anim(t_xenv x, t_map map)
+t_error ft_draw_anim(t_xenv x, t_game g)
 {
 	t_error err;
 	static size_t frame_n;
@@ -49,11 +20,11 @@ t_error ft_draw_anim(t_xenv x, t_map map)
 	if (frame_n >= 12)
 		frame_n = 0;
 
-	err = ft_draw_frame(x, map, PATROL, frame_n);
+	err = ft_draw_frame(x, g.map, PATROL, frame_n);
 	if (err)
 		return (ft_error_patrol(err));
 
-	err = ft_draw_frame(x, map, COLLECTIBLE, frame_n);
+	err = ft_draw_frame(x, g.map, COLLECTIBLE, frame_n);
 	if (err)
 		return (ft_error_coll(err));
 
