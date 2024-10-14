ccc3f9b3d4023e4b0b2d86a851e014b4e78c2919
adryeeel <adryelrbrz+github@gmail.com>
2024-10-14 17:38:25 +0100

    Remove avatar's death frameset free

--- srcs/win/xenv/ft_xenv_free.c
+++ srcs/win/xenv/ft_xenv_free.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 23:04:50 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/13 00:48:19 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/14 17:38:15 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,6 @@ static void ft_sprite_free(t_xenv x)
 
 	if (x.coll.id)
 		ft_ximg_free(x.display, x.coll);
-
-	if (x.death.id)
-		ft_ximg_free(x.display, x.death);
 }
 
 void ft_xenv_free(t_xenv x)
