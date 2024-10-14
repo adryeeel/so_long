221942176c2a056b3b1fe00510a5a9d8c58e5f47
adryeeel <adryelrbrz+github@gmail.com>
2024-10-14 17:37:21 +0100

    Update function call parameters

--- srcs/win/draw/ft_draw_anim.c
+++ srcs/win/draw/ft_draw_anim.c
@@ -6,13 +6,13 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 00:39:11 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/14 16:35:08 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/14 17:36:02 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
 
-t_error ft_draw_anim(t_xenv x, t_game g)
+t_error ft_draw_anim(t_xenv x, t_map map)
 {
 	t_error err;
 	static size_t frame_n;
@@ -20,11 +20,11 @@ t_error ft_draw_anim(t_xenv x, t_game g)
 	if (frame_n >= 12)
 		frame_n = 0;
 
-	err = ft_draw_frame(x, g.map, PATROL, frame_n);
+	err = ft_draw_frame(x, map, PATROL, frame_n);
 	if (err)
 		return (ft_error_patrol(err));
 
-	err = ft_draw_frame(x, g.map, COLLECTIBLE, frame_n);
+	err = ft_draw_frame(x, map, COLLECTIBLE, frame_n);
 	if (err)
 		return (ft_error_coll(err));
 
