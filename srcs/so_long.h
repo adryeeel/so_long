d1bc71e59a1c3d00f3343f1e6e6ce111005e1160
adryeeel <adryelrbrz+github@gmail.com>
2024-10-15 17:53:00 +0100

    Add macro TEXT_IMG_PATH and NUMBER_IMG_PATH for counter rendering

--- srcs/so_long.h
+++ srcs/so_long.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 18:48:22 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/14 14:56:08 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/15 01:37:53 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,9 @@
 #define SPACE_IMG_PATH "assets/imgs/space.xpm"
 #define PATROL_IMG_PATH "assets/imgs/patrol.xpm"
 #define AVATAR_IMG_PATH "assets/imgs/avatar.xpm"
+#define TEXT_IMG_PATH "assets/imgs/move-text.xpm"
 #define COLL_IMG_PATH "assets/imgs/collectible.xpm"
+#define NUMBER_IMG_PATH "assets/imgs/number-text.xpm"
 #define DEATH_IMG_PATH "assets/imgs/avatar-death.xpm"
 #define LAVATAR_IMG_PATH "assets/imgs/avatar-left.xpm"
 
