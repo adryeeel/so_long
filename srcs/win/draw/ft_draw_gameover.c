f5e944507f65789783c0a51e5fcd73b31e905875
adryeeel <adryelrbrz+github@gmail.com>
2024-10-17 17:57:59 +0100

    Execute the game over animation and restart

---
+++ srcs/win/draw/ft_draw_gameover.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_draw_gameover.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 14:31:29 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/10/17 17:29:54 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../so_long.h"
+
+#define MS_500 6
+
+static bool ft_delay(size_t frames)
+{
+	static unsigned total;
+
+	if (total <= frames)
+	{
+		total++;
+		return (true);
+	}
+	return (false);
+}
+
+void ft_draw_gameover(t_xenv x)
+{
+	if (ft_delay(MS_500))
+		return;
+
+	if (ft_draw_fadeout(x))
+		return;
+
+	// ft_game_restart();
+}
