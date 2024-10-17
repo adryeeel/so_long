48a6ce7c446b167f65e313a2d858f8f8daea3666
adryeeel <adryelrbrz+github@gmail.com>
2024-10-17 18:38:57 +0100

    Restart game after fade out

--- srcs/win/draw/ft_draw_gameover.c
+++ srcs/win/draw/ft_draw_gameover.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:31:29 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/17 17:29:54 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/17 18:37:22 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ static bool ft_delay(size_t frames)
 	return (false);
 }
 
-void ft_draw_gameover(t_xenv x)
+void ft_draw_gameover(t_xenv x, t_game *g)
 {
 	if (ft_delay(MS_500))
 		return;
@@ -34,5 +34,5 @@ void ft_draw_gameover(t_xenv x)
 	if (ft_draw_fadeout(x))
 		return;
 
-	// ft_game_restart();
+	ft_game_restart(x, g);
 }
