5102b4304737b50ed442e41ca98f215412a66e9a
adryeeel <adryelrbrz+github@gmail.com>
2024-10-17 17:56:40 +0100

    Add logic to draw death sprite only once

--- srcs/win/draw/ft_draw_death.c
+++ srcs/win/draw/ft_draw_death.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 22:23:16 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/16 00:52:09 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/17 17:37:18 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ t_error ft_draw_death(t_xenv x, t_game g)
 {
 	t_ximg tex;
 	t_error err;
+	static bool was_drawn;
+
+	if (was_drawn)
+		return (OK);
+	was_drawn = true;
 
 	err = ft_ximgf_setup(x.display, &tex, DEATH_IMG_PATH);
 	if (err)
@@ -23,10 +28,7 @@ t_error ft_draw_death(t_xenv x, t_game g)
 
 	ft_draw_background(x.display, &tex);
 
-	ft_ximg_copy(&x.scene, tex, (t_coord){
-		g.avatar.x * TILE_SIZE,
-		g.avatar.y * TILE_SIZE,
-	});
+	ft_draw_at(&x.scene, tex, (t_coord){g.avatar.x, g.avatar.y});
 
 	ft_ximg_free(x.display, tex);
 	return (OK);
