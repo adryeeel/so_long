029eb4d4dd6f3abe18a478326b1cc531d43ed019
adryeeel <adryelrbrz+github@gmail.com>
2024-10-17 17:54:50 +0100

    Increase one tile in Y coordinate for moves label

--- srcs/win/draw/ft_draw_at.c
+++ srcs/win/draw/ft_draw_at.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 17:07:19 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/07 17:10:51 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/17 17:35:59 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void ft_draw_at(t_ximg *dst, t_ximg src, t_coord coord)
 {
 	ft_ximg_copy(dst, src, (t_coord){
 		coord.x * TILE_SIZE, 
-		coord.y * TILE_SIZE
+		(coord.y + 1) * TILE_SIZE
 	});
 }
 
