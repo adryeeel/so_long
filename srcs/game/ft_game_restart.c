92247edfb2cc2c4f4355f066d2ab81cc2a4cb849
adryeeel <adryelrbrz+github@gmail.com>
2024-10-18 14:40:28 +0100

    Improve restart game logic, reseting variables and environment

--- srcs/game/ft_game_restart.c
+++ srcs/game/ft_game_restart.c
@@ -6,20 +6,30 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 18:09:45 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/17 18:30:37 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/18 14:39:13 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-void ft_game_restart(t_xenv x, t_game *g)
+void ft_game_restart(t_xenv *x, t_game *g)
 {
+	t_coord start;
+
+	start = ft_map_search(g->map, AVATAR, (t_coord){1, 1});
+	g->avatar.x = start.x;
+	g->avatar.y = start.y;
+	g->avatar.orient = RIGHT;
+
 	g->moves = 0;
 	g->avatar.died = false;
-	
-	// clear counter digits
 
-	ft_win_draw(x, *g);
+	x->fade = false;
+
+	// Restart coins too
+	// clear counter digits
+	// Fix avatar start position bug
+
+	ft_win_draw(*x, *g);
 	ft_draw_anim(x, g);
 }
-
