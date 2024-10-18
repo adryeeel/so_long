dffe21861df37ecff9c72cbf0de4630cb918519d
adryeeel <adryelrbrz+github@gmail.com>
2024-10-18 19:25:56 +0100

    Reset collectible components

--- srcs/game/ft_game_restart.c
+++ srcs/game/ft_game_restart.c
@@ -6,11 +6,28 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 18:09:45 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/18 14:39:13 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/18 19:24:33 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
+
+static void ft_reset_collectible(t_game *g)
+{
+	size_t x;
+	size_t y;
+
+	y = -1;
+	while (++y < g->map.height)
+	{
+		x = -1;
+		while (++x < g->map.width)
+		{
+			if (g->map.grid[y][x] == COLLECTED)
+				g->map.grid[y][x] = COLLECTIBLE;
+		}
+	}
+}
 
 void ft_game_restart(t_xenv *x, t_game *g)
 {
@@ -26,10 +43,7 @@ void ft_game_restart(t_xenv *x, t_game *g)
 
 	x->fade = false;
 
-	// Restart coins too
-	// clear counter digits
-	// Fix avatar start position bug
-
+	ft_reset_collectible(g);
 	ft_win_draw(*x, *g);
 	ft_draw_anim(x, g);
 }
