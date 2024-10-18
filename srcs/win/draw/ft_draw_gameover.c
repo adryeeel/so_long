0e23451ae56caee5ee0b81a6c3753eeb5cf85546
adryeeel <adryelrbrz+github@gmail.com>
2024-10-18 19:27:51 +0100

    Remove delay function

--- srcs/win/draw/ft_draw_gameover.c
+++ srcs/win/draw/ft_draw_gameover.c
@@ -6,31 +6,14 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:31:29 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/18 14:30:14 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/18 18:41:25 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
 
-#define MS_500 6
-
-static bool ft_delay(size_t frames)
-{
-	static unsigned total;
-
-	if (total <= frames)
-	{
-		total++;
-		return (true);
-	}
-	return (false);
-}
-
 void ft_draw_gameover(t_xenv *x, t_game *g)
 {
-	if (ft_delay(MS_500))
-		return;
-
 	if (ft_draw_fadeout(*x))
 		return;
 
