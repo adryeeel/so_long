c204f87a38f815a8c5df3b44b93f170e836a3cff
adryeeel <adryelrbrz+github@gmail.com>
2024-10-20 00:42:42 +0100

    Add GOVER_IMG_PATH macro for game over text sprite

--- srcs/so_long.h
+++ srcs/so_long.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 18:48:22 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/19 13:23:33 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/19 15:12:20 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@
 #define PATROL_IMG_PATH "assets/imgs/patrol.xpm"
 #define AVATAR_IMG_PATH "assets/imgs/avatar.xpm"
 #define NUMBER_IMG_PATH "assets/imgs/number.xpm"
+#define GOVER_IMG_PATH "assets/imgs/game-over.xpm"
 #define COLL_IMG_PATH "assets/imgs/collectible.xpm"
 #define DEATH_IMG_PATH "assets/imgs/avatar-death.xpm"
 #define LAVATAR_IMG_PATH "assets/imgs/avatar-left.xpm"
