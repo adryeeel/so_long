c81d712f53337d10051f5d75515ab217b5ebe325
adryeeel <adryelrbrz+github@gmail.com>
2024-10-20 00:28:38 +0100

    Restart game variables and graphics

--- srcs/game/ft_game_restart.c
+++ srcs/game/ft_game_restart.c
@@ -6,44 +6,26 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 18:09:45 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/18 19:24:33 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/20 00:22:26 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
-
-static void ft_reset_collectible(t_game *g)
-{
-	size_t x;
-	size_t y;
-
-	y = -1;
-	while (++y < g->map.height)
-	{
-		x = -1;
-		while (++x < g->map.width)
-		{
-			if (g->map.grid[y][x] == COLLECTED)
-				g->map.grid[y][x] = COLLECTIBLE;
-		}
-	}
-}
 
 void ft_game_restart(t_xenv *x, t_game *g)
 {
 	t_coord start;
 
 	start = ft_map_search(g->map, AVATAR, (t_coord){1, 1});
+	
+	g->moves = 0;
 	g->avatar.x = start.x;
 	g->avatar.y = start.y;
-	g->avatar.orient = RIGHT;
-
-	g->moves = 0;
 	g->avatar.died = false;
+	g->avatar.orient = RIGHT;
+	x->allow_move = true;
 
-	x->fade = false;
-
-	ft_reset_collectible(g);
+	ft_map_restart(&g->map);
 	ft_win_draw(*x, *g);
 	ft_draw_anim(x, g);
 }
