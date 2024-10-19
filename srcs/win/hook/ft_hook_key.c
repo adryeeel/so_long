e2d7bdddb6cf2118c67820ee255ceb39a4383703
adryeeel <adryelrbrz+github@gmail.com>
2024-10-20 00:37:54 +0100

    Add control condition to determine if avatar's movement is either allowed or not

--- srcs/win/hook/ft_hook_key.c
+++ srcs/win/hook/ft_hook_key.c
@@ -6,49 +6,26 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 20:17:06 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/17 17:44:38 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/19 17:15:33 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../so_long.h"
 
-#define XK_LATIN1
-#define XK_MISCELLANY
-
-#include <X11/keysymdef.h>
-
 int ft_hook_key(int key, void *param)
 {
 	t_xenv *x;
 	t_game *g;
-	t_error err;
 
 	x = ((t_param *)param)->x;
 	g = ((t_param *)param)->g;
-
-	if (key == XK_Escape)
-		mlx_loop_end(x->display);
-
-	if (key == XK_W || key == XK_w)
-		ft_move_up(g);
-
-	if (key == XK_A || key == XK_a)
-		ft_move_left(g);
-
-	if (key == XK_S || key == XK_s)
-		ft_move_down(g);
-
-	if (key == XK_D || key == XK_d)
-		ft_move_right(g);
-
-	err = ft_draw_move(*x, *g);
-	if (err)
+	
+	if (x->allow_move)
 	{
-		ft_error_print(err);
-		mlx_loop_end(x->display);
+		ft_key_move(*x, g, key);
+		return (0);
 	}
 
-	mlx_put_image_to_window(x->display, x->window, x->scene.id, 0, 0);
-
+	ft_key_exit(*x, key);
 	return (0);
 }
