f9916b58a21c767f7deb42799b2b68f75101d6d4
adryeeel <adryelrbrz+github@gmail.com>
2024-10-29 20:25:03 +0000

    Update error return to combine render error and ximg setup

--- srcs/win/ft_win_draw.c
+++ srcs/win/ft_win_draw.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 00:49:08 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/15 17:59:40 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/24 00:06:12 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,23 @@ t_error ft_win_draw(t_xenv x, t_game g)
 
 	err = ft_draw_counter(x, g);
 	if (err)
-		return (ft_error_counter(err));
+		return (ft_error(err, ERR_RENDCOUNT));
 
 	err = ft_draw_comp(x, g, WALL);
 	if (err)
-		return (ft_error_wall(err));
+		return (ft_error(err, ERR_RENDWALL));
 
 	err = ft_draw_comp(x, g, SPACE);
 	if (err)
-		return (ft_error_space(err));
+		return (ft_error(err, ERR_RENDSPACE));
 
 	err = ft_draw_comp(x, g, AVATAR);
 	if (err)
-		return (ft_error_avatar(err));
+		return (ft_error(err, ERR_RENDAVATAR));
 
 	err = ft_draw_comp(x, g, EXIT_POINT);
 	if (err)
-		return (ft_error_exit(err));
+		return (ft_error(err, ERR_RENDEXIT));
 
 	return (OK);
 }
