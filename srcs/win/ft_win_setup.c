54cbdfad6d2259f14813de57a90eab3e614efffe
adryeeel <adryelrbrz+github@gmail.com>
2024-10-29 20:25:25 +0000

    Update error type for window creation

--- srcs/win/ft_win_setup.c
+++ srcs/win/ft_win_setup.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 22:12:35 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/09 22:22:14 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/10/28 19:53:14 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_error ft_win_setup(t_xenv *x, t_game g, t_param *args)
 	x->window = mlx_new_window(x->display, WIN_WH, WIN_HT, "so_long");
 
 	if (!x->window)
-		return (ERR_WIN_CREATE);
+		return (ERR_XSRV_WIN);
 
 	mlx_key_hook(x->window, ft_hook_key, args);
 	mlx_close_hook(x->window, ft_hook_close, x);
