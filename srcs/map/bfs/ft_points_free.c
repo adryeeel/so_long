d769dc0431305a714710fbcfeeb10abdefe9989e
adryeeel <adryelrbrz+github@gmail.com>
2024-11-03 17:43:47 +0000

    Free the points queue

--- srcs/map/bfs/ft_points_free.c
+++ srcs/map/bfs/ft_points_free.c
@@ -6,11 +6,11 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 22:43:18 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/24 10:31:01 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/11/01 00:42:43 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../so_long.h"
+#include "../../so_long.h"
 
 void ft_points_free(t_queue points, t_coord *pos)
 {
