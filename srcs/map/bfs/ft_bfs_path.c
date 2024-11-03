915d4e003f9bb8960b3b81b82634fc92f8c7efa6
adryeeel <adryelrbrz+github@gmail.com>
2024-11-03 17:40:39 +0000

    Process the BFS algorithm

--- srcs/map/bfs/ft_bfs_path.c
+++ srcs/map/bfs/ft_bfs_path.c
@@ -1,125 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_map_bfs.c                                       :+:      :+:    :+:   */
+/*   ft_bfs_path.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 00:03:09 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/28 01:02:25 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/11/02 04:57:09 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../so_long.h"
+#include "../../so_long.h"
 
-#define TOTAL_DIRECTIONS 4
-
-static t_error ft_bfs_setup(t_map map, t_coord start, int ***visited, t_coord **sp)
+t_error ft_bfs_path(t_map map, t_coord start, t_coord end)
 {
-	*visited = ft_matrix_alloc(map.width, map.height);
-
-	if (!*visited)
-		return (ERR_MAP_PATH_ALLOC);
-
-	(*visited)[start.y][start.x] = true;
-
-	*sp = ft_calloc(1, sizeof(t_coord));
-
-	if (!*sp)
-		return (ft_matrix_free(*visited), ERR_MAP_PATH_ALLOC);
-
-	(*sp)->x = start.x;
-	(*sp)->y = start.y;
-
-	return (OK);
-}
-
-static bool ft_bfs_isvalid(t_map map, t_coord pos, int **visited)
-{
-	size_t ok;
-
-	ok = 0;
-	if (pos.x >= 0 && pos.x < (int)map.width)
-		ok++;
-
-	if (pos.y >= 0 && pos.y < (int)map.height)
-		ok++;
-
-	if (!visited[pos.y][pos.x])
-		ok++;
-
-	if (map.grid[pos.y][pos.x] != WALL)
-		ok++;
-
-	return (ok == 4);
-}
-
-static t_error ft_bfs_edges(t_map map, t_queue *q, t_coord curr, int **visited)
-{
-	int i;
-	t_coord *pos;
-	int **directions;
-
-	i = -1;
-	directions = ft_dirs_setup();
-	while (++i < TOTAL_DIRECTIONS)
-	{
-		pos = ft_calloc(1, sizeof(t_coord));
-
-		if (!pos)
-			return (ft_matrix_free(directions), ERR_MAP_PATH_ALLOC);
-
-		pos->x = curr.x + directions[i][0];
-		pos->y = curr.y + directions[i][1];
-
-		if (ft_bfs_isvalid(map, *pos, visited))
-		{
-			ft_enqueue(q, pos);
-			visited[pos->y][pos->x] = true;
-			continue;
-		}
-		free(pos);
-	}
-
-	ft_matrix_free(directions);
-	return (OK);
-}
-
-t_error ft_map_bfs(t_map map, t_coord start, t_coord end)
-{
-	t_queue q;
 	t_error err;
-	t_coord *sp;
-	t_coord *pos;
 	int **visited;
+	t_queue points;
 
-	sp = NULL;
 	visited = NULL;
 
-	err = ft_bfs_setup(map, start, &visited, &sp);
+	err = ft_bfs_setup(map, start, &points, &visited);
 	if (err)
 		return (err);
 
-	ft_queue_init(&q);
-	ft_enqueue(&q, sp);
-
-	while (q.length > 0)
-	{
-		pos = ft_dequeue(&q);
-
-		if (pos->x == end.x && pos->y == end.y)
-			return (ft_points_free(q, pos), ft_matrix_free(visited), OK);
-
-		err = ft_bfs_edges(map, &q, *pos, visited);
-		if (err)
-			break;
-
-		err = ERR_MAP_PATH;
-		ft_free((void **)&pos);
-	}
-
-	ft_points_free(q, pos);
-	ft_matrix_free(visited);
+	err = ft_bfs_process(map, end, &points, visited);
+	ft_bfs_cleanup(points, visited);
 
 	return (err);
 }
