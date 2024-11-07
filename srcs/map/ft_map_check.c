c5ed3d922d37ae34c94c785b0582203b395254f6
adryeeel <adryelrbrz+github@gmail.com>
2024-11-07 23:58:41 +0000

    Add verification for empty maps

--- srcs/map/ft_map_check.c
+++ srcs/map/ft_map_check.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 15:03:21 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/10/09 22:50:15 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/11/07 23:56:33 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 t_error ft_map_check(t_map map)
 {
 	t_error err;
+
+	if (ft_check_isempty(map))
+		return (ERR_MAP_ISEMPTY);
 
 	if (!ft_check_isrect(map))
 		return (ERR_MAP_RECT);
