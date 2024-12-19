b392f4dfab8cf46d7bbcb3b76cfb98428143b4b8
adryeeel <adryelrbrz+github@gmail.com>
2024-12-19 03:40:16 +0000

    WIP: Make enemies move in the map by randomly changing their position

---
+++ srcs/map/ft_map_patrol.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_map_patrol.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 11:57:44 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/11/08 12:33:40 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+
+// Get a random patrol
+// Move to a random valid position
+
+void ft_get_patrol(t_map map)
+{
+	(void)map;
+}
+
+void ft_map_patrol(t_map map)
+{
+	(void)map;
+	printf("Move\n");
+}
